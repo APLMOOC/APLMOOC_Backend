f69ab716d6f084f6e4d5c4e6b8fc680119d1ed3e
PixelSergey <sergey.ichtchenko@gmail.com>
2024-07-18 22:46:58 +0100

    Get secret key from environment variable

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,10 +4,9 @@ import os
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY="dev",
+        SECRET_KEY=os.getenv("FLASK_SECRET_KEY", "dev"),
         DATABASE=os.path.join(app.instance_path, "points.sqlite"),
     )
-    app.config.from_pyfile("config.py", silent=True)
 
     try:
         os.makedirs(app.instance_path)

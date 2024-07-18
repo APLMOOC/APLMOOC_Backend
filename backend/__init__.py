4843df0c0c7c113ad2fc8f5ad90e62b8d7f916b5
PixelSergey <sergey.ichtchenko@gmail.com>
2024-07-18 22:23:03 +0100

    Change single to double quotes

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,8 +4,8 @@ import os
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'points.sqlite'),
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "points.sqlite"),
     )
     app.config.from_pyfile("config.py", silent=True)
 

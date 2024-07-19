dd4735dcb836004b20eae4ab3ad4c199c24f1567
PixelSergey <sergey.ichtchenko@gmail.com>
2024-07-19 11:28:02 +0100

    Generate secret key in code

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,17 +1,29 @@
 from flask import Flask
 import os
+import secrets
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY=os.getenv("FLASK_SECRET_KEY", "dev"),
-        DATABASE=os.path.join(app.instance_path, "points.sqlite"),
-    )
 
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    keyfile = os.path.join(app.instance_path, ".flask_secret")
+    key = ""
+    try:
+        with open(keyfile, "r") as f:
+            key = f.readline().strip()
+    except FileNotFoundError:
+        key = secrets.token_hex(64)
+        with open(keyfile, "w") as f:
+            f.write(key)
+
+    app.config.from_mapping(
+        SECRET_KEY=key,
+        DATABASE=os.path.join(app.instance_path, "points.sqlite"),
+    )
 
     from . import database
     database.init_app(app)
